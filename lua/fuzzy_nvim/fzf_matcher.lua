4b1e1d1bd7bc93eafcbcb72b486475589a12747c
Anshuman <amedhi@connect.ust.hk>
2023-04-13 15:23:28 +0800

    lines can come with metadata

--- lua/fuzzy_nvim/fzf_matcher.lua
+++ lua/fuzzy_nvim/fzf_matcher.lua
@@ -8,11 +8,12 @@ M.filter = function(_, pattern, lines, case_mode)
 	local slab = fzf.allocate_slab()
 	local pattern_obj = fzf.parse_pattern(pattern, case_mode, true)
 
-	for _, line in ipairs(lines) do
+	for index, line_tbl in ipairs(lines) do
+		local line = line_tbl[1] or line_tbl
 		local score = fzf.get_score(line, pattern_obj, slab)
 		local positions = fzf.get_pos(line, pattern_obj, slab)
-		if (positions and #positions > 0) then
-			table.insert(ans, {line, positions, score})
+		if positions and #positions > 0 then
+			table.insert(ans, { line_tbl, positions, score, index })
 		end
 	end
 	table.sort(ans, function(a, b) return a[3] > b[3] end)
