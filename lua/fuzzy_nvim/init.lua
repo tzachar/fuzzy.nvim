e29d96b5fab9bb9f613c86bd54ea9b7ef2de61bb
UnderCooled <161503+UnderCooled@users.noreply.github.com>
2024-05-17 00:46:02 +0800

    Add telescope-zf-native.nvim support. (#4)
    * Add telescope-zf-native.nvim support.
    
    * Fix README.md description about zf installation using packer.
    
    * Fix the ugly nest-if I made.
    
    ---------
    
    Co-authored-by: unc0 <unc0@users.noreply.github.com>

--- lua/fuzzy_nvim/init.lua
+++ lua/fuzzy_nvim/init.lua
@@ -1,12 +1,17 @@
 local has_fzf, matcher = pcall(require, 'fuzzy_nvim.fzf_matcher')
-if not has_fzf then
-	local has_fzy, fzy_matcher = pcall(require, 'fuzzy_nvim.fzy_matcher')
-	if has_fzy then
-		matcher = fzy_matcher
-	else
-		vim.notify('fuzzy.nvim: Cannot find neither fzy nor fzf. Please install either')
-		return
-	end
+if has_fzf then
+   return matcher
 end
 
-return matcher
+local has_fzy, fzy_matcher = pcall(require, 'fuzzy_nvim.fzy_matcher')
+if has_fzy then
+   return fzy_matcher 
+end
+
+local has_zf, zf_matcher = pcall(require, 'fuzzy_nvim.zf_matcher')
+if has_zf then
+   return zf_matcher 
+end
+
+vim.notify('fuzzy.nvim: Cannot find neither fzy nor fzf or zf. Please install any of them')
+return
